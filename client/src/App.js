4004ddb1a59d7df93a717520051034c42816a8d8
Atul Pandey <atulpandey@Atuls-MacBook-Air.local>
2023-11-25 07:55:31 -0800

    commit 3 - atul - landing page with logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import { EtHero, MuteIcon, UnMuteIcon } from "./img/imgAssets"
+import { EtHero, MuteIcon, UnMuteIcon, EtLogo } from "./img/imgAssets"
 import { useState } from "react"
 
 function App() {
 
-  const [isMute, setIsMute] = useState(false)
+  const [isMute, setIsMute] = useState(true)
 
   const handleClick = () => {
     setIsMute(!isMute)
@@ -14,11 +14,12 @@ function App() {
 
       <div id='Nav' className="absolute top-0 left-0 right-0 z-50 flex justify-between items-center p-5 bg-transparent text-white">
         <div className="flex justify-start items-center">
-          <p className="m-2 p-2 cursor-pointer">Home</p>
+          {/* <p className="m-2 p-2 cursor-pointer">Home</p> */}
+          <img src={EtLogo} alt="logo" className="h-9 w-18 cursor-pointer m-2 p-2" onClick={""} />
           <p className="m-2 p-2 cursor-pointer">About</p>
-          <p className="m-2 p-2 cursor-pointer">Services</p>
+          <p className="m-2 p-2 cursor-pointer">Trails</p>
           <p className="m-2 p-2 cursor-pointer">Blogs</p>
-          <p className="m-2 p-2 cursor-pointer">Contact Us</p>
+          {/* <p className="m-2 p-2 cursor-pointer">Contact Us</p> */}
         </div>
 
         <div className=" flex justify-start items-center pr-5">
@@ -40,12 +41,7 @@ function App() {
           className="w-full h-full object-cover"
           src={EtHero}
         />
-        {/* Call to Action Button */}
-        {/* <div id='Get Started Btn Div' className="absolute right-12 top-24">
-          <button id='Get Started Btn' className="bg-transparent text-orange-600 text-xl font-semibold py-1 px-4 border border-gray-500 rounded-full">
-            Get Started
-          </button>
-        </div> */}
+      
       </div>
           
       </div>
--- client/src/img/imgAssets.js
+++ client/src/img/imgAssets.js
@@ -1,10 +1,14 @@
 import EtHero from './videos/EtNoLogo.m4v'
+import EtHero1 from './videos/EtWeb1.mp4'
 import MuteIcon from './muteIcon.svg'
 import UnMuteIcon from './unmuteIcon.svg'
+import EtLogo from './etLogo.svg'
 
 
 export {
     EtHero,
+    EtHero1,
     MuteIcon,
     UnMuteIcon,
+    EtLogo,
 }
